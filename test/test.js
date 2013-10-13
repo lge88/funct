e6db67cd05af24dcd56d237af868edbf7c841cdc
Li Ge <lge@ucsd.edu>
2013-10-12 23:36:23 -0700

    Update at Sat Oct 12 23:36:23 PDT 2013

--- test/test.js
+++ test/test.js
@@ -4,16 +4,23 @@ var expect = require( 'expect.js' );
 
 describe( 'funct', function() {
 
-  it( 'should return funct', function() {
-    function foo( a, b, c ) {
-      return ( a + b ) * c;
+  it( 'should return wrapped funct', function() {
+    function foo( a, b, c, d ) {
+      return ( a + b ) * ( c - d );
     }
 
+    var _ = void 0;
     var fooFunct = funct( foo );
     expect( fooFunct ).to.be.a( 'function' );
     expect( fooFunct( 1 ) ).to.be.a( 'function' );
     expect( fooFunct( 1 )( 2 ) ).to.be.a( 'function' );
-    expect( fooFunct( 1 )( 2 )( 3 ) ).to.be( 9 );
+    expect( fooFunct( 1 )()( 2 ) ).to.be.a( 'function' );
+    expect( fooFunct( 1, _, 2 )( 3 ) ).to.be.a( 'function' );
+    expect( fooFunct( _, _, 2 )( 3 )( 1, 4 ) ).to.be( foo( 3, 1, 2, 4 ) );
+    expect( fooFunct.apply( null, [  _, _, 2 ] )( 3 )( 1, 4 ) ).to.be( foo( 3, 1, 2, 4 ) );
+    expect( fooFunct( 1 )( 2 )( 3 )( 4 ) ).to.be( foo( 1, 2, 3, 4 ) );
+    expect( fooFunct( 1 )( 2 )()()( 3 )( 4 ) ).to.be( foo( 1, 2, 3, 4 ) );
+    expect( fooFunct()()()( 1 )( 2 )( 3 )( 4 ) ).to.be( foo( 1, 2, 3, 4 ) );
   } );
 
 } );
